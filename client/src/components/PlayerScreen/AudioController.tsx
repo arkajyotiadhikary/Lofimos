721e552eaf272b08530a10793735310f3d12dd49
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-03-18 14:06:47 +0530

    Refactor: Simplify AudioController component by extracting smaller components and improving readability

--- client/src/components/PlayerScreen/AudioController.tsx
+++ client/src/components/PlayerScreen/AudioController.tsx
@@ -1,59 +1,54 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Slider from "@react-native-community/slider";
-import { FontAwesome } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { FontAwesome6 } from "@expo/vector-icons";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import styles from "../../styles/PlayerScreen/AudioController";
+import TrackPlayer, { useProgress } from "react-native-track-player";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import { setCurrentAudioControls } from "../../features/song/songSlice";
+import CustomSlider from "./audio controller/CustomSlider";
+import TimeDisplay from "./audio controller/TimeDisplay";
+import PlaybackControls from "./audio controller/PlaybackControls";
 
 const AudioController = () => {
+    const dispatch = useDispatch();
+    const { isPlaying } = useSelector(
+        (state: RootState) => state.songControlsReducer
+    );
+    const [sliderValue, setSliderValue] = useState(0);
+    const { position, duration } = useProgress();
+
+    useEffect(() => {
+        if (duration && position) {
+            setSliderValue(position / duration);
+        }
+    }, [position, duration]);
+
+    const handleSeek = (value: number) => {
+        TrackPlayer.seekTo(value * duration);
+    };
+
+    const handleSkip = () => {
+        TrackPlayer.skipToNext();
+    };
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            TrackPlayer.pause();
+        } else {
+            TrackPlayer.play();
+        }
+        dispatch(setCurrentAudioControls({ isPlaying: !isPlaying }));
+    };
+
     return (
         <View>
-            <Slider
-                style={styles.slider}
-                thumbTintColor="black"
-                maximumTrackTintColor="black"
-                minimumTrackTintColor="black"
+            <CustomSlider value={sliderValue} onSlidingComplete={handleSeek} />
+            <TimeDisplay position={position} duration={duration} />
+            <PlaybackControls
+                isPlaying={isPlaying}
+                onPlayPause={handlePlayPause}
+                onSkip={handleSkip}
             />
-            <View style={styles.timelineValues}>
-                {/* current position */}
-                <Text>0.02</Text>
-                {/* total position */}
-                <Text>4.03</Text>
-            </View>
-            <View style={styles.timelineControls}>
-                <FontAwesome6
-                    name="shuffle"
-                    size={24}
-                    color="black"
-                    style={styles.icon}
-                />
-                <AntDesign
-                    name="stepbackward"
-                    size={24}
-                    color="black"
-                    style={styles.icon}
-                />
-                <FontAwesome
-                    name="play"
-                    size={24}
-                    color="black"
-                    style={styles.icon}
-                />
-                <AntDesign
-                    name="stepforward"
-                    size={24}
-                    color="black"
-                    style={styles.icon}
-                />
-                <FontAwesome
-                    name="repeat"
-                    size={24}
-                    color="black"
-                    style={styles.icon}
-                />
-            </View>
         </View>
     );
 };
